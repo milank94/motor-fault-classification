65803a940bc028bc227f3eea9e64ef368a1576c1
Milan Kordic <milan.kordic94@gmail.com>
2021-10-23 13:19:02 -0600

    Reformat eda module

--- eda/signal_processing_experiment.py
+++ eda/signal_processing_experiment.py
@@ -1,30 +1,17 @@
-# +
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-
-path_parent = os.path.dirname(os.getcwd())
-os.chdir(path_parent)
-# -
-
 from data_acquisition.main import get_data
 from data_processing.utils import _downSampler, _FFT, _dataScaler
 from eda.utils import time_plot, fft_plot
-import data_processing.data_processing_config as config
 
 dataset = get_data()
 
-# ## Data Plotting
-
+# Data Plotting
 data_n = dataset.normal.copy()
 data_horizontal = dataset.horizontal.copy()
 data_imbalance = dataset.imbalance.copy()
 
 time_plot(data_n[0], 0, 250000)
 
-# ## Data Resampling
-
+# Data Resampling
 data_n_resampled = _downSampler(data_n, 0, 100)
 data_n_resampled_fft = _FFT(data_n_resampled)
 fft_plot(data_n_resampled_fft[0])
@@ -37,9 +24,6 @@ data_imbalance_resampled = _downSampler(data_imbalance, 0, 100)
 data_imbalance_resampled_fft = _FFT(data_imbalance_resampled)
 fft_plot(data_imbalance_resampled_fft[0])
 
-# ## Data Scaling
-
+# Data Scaling
 data_n_resampled_scaled = _dataScaler(data_n_resampled)
 time_plot(data_n_resampled_scaled[0], 0, 2500)
-
-
