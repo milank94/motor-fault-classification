dc99186c9d4212ab6a1e5141bd3a0fb9360c4095
Milan Kordic <milan.kordic94@gmail.com>
2021-10-23 13:08:19 -0600

    Reformat data acquisition module

--- data_acquisition/data_acquisition_config.py
+++ data_acquisition/data_acquisition_config.py
@@ -3,10 +3,9 @@ Any config specific to data acquisition step goes here.
 """
 
 from pathlib import Path
-from pyprojroot import here as get_project_root
 import glob
 
-INPUT_DATA_DIR = get_project_root() / Path('data_acquisition/data')
+INPUT_DATA_DIR = Path('./data_acquisition/data')
 
 NORMAL_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/normal/*.csv')
 HORI_MIS_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/horizontal/*.csv')
@@ -14,4 +13,3 @@ VERT_MIS_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/vertical/*.csv')
 IMBALANCE_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/imbalance/*.csv')
 OVERHANG_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/overhang/*.csv')
 UNDERHANG_FILE_NAMES = glob.glob(str(INPUT_DATA_DIR)+'/underhang/*.csv')
-
