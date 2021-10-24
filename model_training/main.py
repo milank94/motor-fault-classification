130987bec2925cfb75a4255725e26154efef4d68
Milan Kordic <milan.kordic94@gmail.com>
2021-10-23 23:36:08 -0600

    Update model training module

--- model_training/main.py
+++ model_training/main.py
@@ -1,34 +1,26 @@
-import pandas as pd
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
-import os
-from data_acquisition.main import get_data
-from data_processing.main import get_train_test_data
+from data_acquisition.main import get_save_data
+from data_processing.main import get_save_train_test_data
 import model_training_config as config
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-
-from keras.models import load_model
-from keras.callbacks import ModelCheckpoint
-
-from sklearn.metrics import accuracy_score
-
-path_parent = os.path.dirname(os.getcwd())
-os.chdir(path_parent)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 logging.basicConfig(level=logging.INFO)
 
-# Model Development
+config.OUTPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
+# Model Development
 # Load the data
-dataset = get_data()
+dataset = get_save_data()
 
 # Process the data and train/test split
-train_test_data = get_train_test_data(dataset)
-
+train_test_data = get_save_train_test_data(dataset)
 
 # Prepare the data
 X_train = np.array(train_test_data.X_train)
@@ -41,11 +33,11 @@ y_test = np.array(train_test_data.y_test)
 model = Sequential()
 model.add(LSTM(config.LSTM_UNITS, input_shape=(X_train.shape[1], X_train.shape[2])))
 model.add(Dense(config.OUTPUT_SIZE, activation=config.ACTIVATION))
-model.summary()
-
+print(model.summary())
 
 # Training the model
-chk = ModelCheckpoint('best_model.pkl', monitor=config.MONITOR, save_best_only=True, mode='auto', verbose=1)
+model_path = config.OUTPUT_DATA_DIR / Path('best_model.pkl')
+chk = ModelCheckpoint(model_path, monitor=config.MONITOR, save_best_only=True, mode='auto', verbose=1)
 model.compile(loss=config.LOSS_FUNCTION, optimizer=config.OPTIMIZER, metrics=['accuracy'])
 hist = model.fit(
     X_train,
@@ -53,12 +45,13 @@ hist = model.fit(
     epochs=config.EPOCHS,
     batch_size=int(X_train.shape[0]),
     callbacks=[chk],
-    validation_split=config.VAL_SPLOT
+    validation_split=config.VAL_SPLIT
     )
 
 # Model Validation
 # Plotting training and validation accuracy per epoch
-fig, axs = plt.subplots(nrows=1, figsize=(11, 9))
+_, axs = plt.subplots(nrows=1, figsize=(11, 9))
+file_location = config.OUTPUT_DATA_DIR / Path('plots/model_accuracy.png')
 plt.rcParams['font.size'] = '14'
 
 for label in (axs.get_xticklabels() + axs.get_yticklabels()):
@@ -71,16 +64,4 @@ axs.set_title('Model Accuracy')
 axs.set_ylabel('Accuracy', fontsize=14)
 axs.set_xlabel('Epoch', fontsize=14)
 plt.legend(['train', 'val'], loc='upper left')
-plt.show()
-
-# Loading the model and checking accuracy on the test data
-model = load_model('best_model.pkl')
-
-test_preds = model.predict_classes(X_test)
-accuracy_score(y_test, test_preds)
-
-# Comparing the actual values versus the predicted values
-data_dict = {0: 'normal', 1: 'horizontal misalignment', 2: 'imbalance'}
-results = pd.DataFrame([y_test, test_preds]).T
-results.columns = ['Actual', 'Prediction']
-results.applymap(lambda x: data_dict[x])
+plt.savefig(file_location)
